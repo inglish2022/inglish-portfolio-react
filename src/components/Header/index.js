8bb4a21b79df5d732aa989de9257634802345e92
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-07-13 10:52:51 -0400

    fix header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Nav from "../Nav";
+// import Nav from "../Nav";
 
 
 
@@ -7,18 +7,49 @@ import Nav from "../Nav";
 
 function Header() {
 
-  return (
-    <header className="header" id="myHeader">
-      <div className='logo'>
-        <h1>
-          <a href="#about">Inglish Foust</a>
-        </h1>
-      </div>
-      <div>
-        <Nav />
-      </div>
-    </header>
-  );
+
+return(
+<header className="header" id="myHeader">
+<div className='logo'>
+  <h1>
+    <div>
+      <ul className="flex-row">
+        <div>
+          <li>
+            <a href="https://github.com/inglish2022">
+              INGLISH FOUST
+              
+            </a>
+          </li>
+        </div>
+        <div className='flex-row'>
+          <li className='mx-2'>
+            <a href="#about">
+              ABOUT ME
+            </a>
+          </li>
+          <li className='mx-2'>
+            <a href="#resume">
+              RESUME
+            </a>
+          </li>
+          <li className='mx-2'>
+            <a href="#portfolio">
+              PORTFOLIO
+            </a>
+          </li>
+          <li className="mx-2">
+            <a href="#contact-form">
+              CONTACT
+            </a>
+          </li>
+        </div>
+      </ul>
+    </div>
+  </h1>
+</div>
+</header>
+);
 }
 
 export default Header;
